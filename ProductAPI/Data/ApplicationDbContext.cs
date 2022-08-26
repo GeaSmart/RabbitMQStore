4bf90f8adb2c6c6dc044643dcf3d7bb00c745002
Gerson Azabache <gerson_dunk@hotmail.com>
2022-08-26 16:19:41 -0500

    model added, folder structure and context added

---
+++ ProductAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductAPI.Models;
+
+namespace ProductAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IConfiguration configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
