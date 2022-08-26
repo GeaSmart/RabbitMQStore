3ec6d623234e8e36203e68dbd2a46b9c896ce291
Gerson Azabache <gerson_dunk@hotmail.com>
2022-08-26 16:40:53 -0500

    services added

---
+++ ProductAPI/Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using ProductAPI.Models;
+
+namespace ProductAPI.Services
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetAll();
+        Task<Product> GetById(int id);
+        Task<Product> Add(Product product);
+        Task<Product> Update(Product product);
+        Task<bool> Delete(int id);
+    }
+}
---
+++ ProductAPI/Services/ProductService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductAPI.Data;
+using ProductAPI.Models;
+
+namespace ProductAPI.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        
+        public async Task<IEnumerable<Product>> GetAll()
+        {
+            return await context.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetById(int id)
+        {
+            return await context.Products.Where(x => x.ProductId == id).FirstOrDefaultAsync();            
+        }
+        public async Task<Product> Add(Product product)
+        {
+            var result = context.Products.Add(product);
+            await context.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public async Task<Product> Update(Product product)
+        {
+            var result = context.Products.Update(product);
+            await context.SaveChangesAsync();
+            return result.Entity;
+        }
+        public async Task<bool> Delete(int id)
+        {
+            var filteredData = await context.Products.Where(x => x.ProductId == id).FirstOrDefaultAsync();
+            var result = context.Remove(filteredData);
+            await context.SaveChangesAsync();
+            return result != null;
+        }
+    }
+}
