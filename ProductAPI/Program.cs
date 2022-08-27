6f0ff0e6cf1a8596e40e619703c069a8cede0d4a
Gerson Azabache <gerson_dunk@hotmail.com>
2022-08-27 01:02:02 -0500

    project services configured

--- ProductAPI/Program.cs
+++ ProductAPI/Program.cs
@@ -1,6 +1,14 @@
+using ProductAPI.Data;
+using ProductAPI.RabbitMQ;
+using ProductAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddScoped<IRabbitMQProducer, RabbitMQProducer>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
